a3bc78211c87c5064e8236bc2619de78faa99c9b
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-07-22 22:21:21 +0300

    Подключить заместителя Надзора и экономику фондов Reality 167

--- talent_entry_v164.py
+++ talent_entry_v164.py
@@ -6,9 +6,11 @@ from boss_audio_v154 import install_boss_audio_v154
 from boss_rules_v163 import install_boss_rules_v163
 from boss_web_v163 import install_boss_web_v163
 from economy_rewards_v164 import install_economy_rewards_v164
+from finance_fund_link_v167 import install_finance_fund_link_v167
 from government_android_webview_hotfix_v166 import install_government_android_webview_hotfix_v166
 from government_creator_sanctions_v164 import install_government_creator_sanctions_v164
 from government_official_voting_v168 import install_government_official_voting_v168
+from government_oversight_deputy_v167 import install_government_oversight_deputy_v167
 from government_release_v165 import install_government_release_v165
 from government_reliability_laws_v167 import install_government_reliability_laws_v167
 
@@ -23,6 +25,10 @@ install_government_creator_sanctions_v164(core)
 install_government_reliability_laws_v167(core)
 # Даёт право голоса всем действующим должностным лицам, включая президента.
 install_government_official_voting_v168(core)
+# Добавляет заместителя главы Надзора, жалобы, проверки, реестр и отчёты.
+install_government_oversight_deputy_v167(core)
+# Связывает акции и ставки вкладов с государственными и частными фондами беседы.
+install_finance_fund_link_v167(core)
 # Финальные правила рейда ставятся после всех старых обёрток боя и героев.
 install_boss_rules_v163(core)
 install_boss_audio_v154()
