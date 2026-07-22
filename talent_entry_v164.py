e84ae22b9dfbe86712b53221fb2d409e17970a55
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-07-22 21:57:55 +0300

    Подключить Reality 167 в рабочий entrypoint

--- talent_entry_v164.py
+++ talent_entry_v164.py
@@ -9,14 +9,17 @@ from economy_rewards_v164 import install_economy_rewards_v164
 from government_android_webview_hotfix_v166 import install_government_android_webview_hotfix_v166
 from government_creator_sanctions_v164 import install_government_creator_sanctions_v164
 from government_release_v165 import install_government_release_v165
+from government_reliability_laws_v167 import install_government_reliability_laws_v167
 
 
 # Финальный государственный слой включает запросы госструктур в казну Reality 165.
 install_government_release_v165(core)
-# Убирает параллельные фоновые загрузки и повторные DDL-запросы в Android WebView.
+# Убирает параллельные фоновые загрузки казны и повторные DDL-запросы.
 install_government_android_webview_hotfix_v166(core)
 # Добавляет владельцу отдельную панель снятия всех санкций в Mini App Правительства.
 install_government_creator_sanctions_v164(core)
+# Объединяет все запросы состояния, чинит обновление и добавляет президентские редакции законов.
+install_government_reliability_laws_v167(core)
 # Финальные правила рейда ставятся после всех старых обёрток боя и героев.
 install_boss_rules_v163(core)
 install_boss_audio_v154()
