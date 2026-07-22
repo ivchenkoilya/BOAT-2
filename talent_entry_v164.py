15bde52f035ed089f0f7241fe42291558c75b9d5
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-07-22 15:49:00 +0300

    Подключить панель снятия санкций в Reality 164

--- talent_entry_v164.py
+++ talent_entry_v164.py
@@ -6,10 +6,13 @@ from boss_audio_v154 import install_boss_audio_v154
 from boss_rules_v163 import install_boss_rules_v163
 from boss_web_v163 import install_boss_web_v163
 from economy_rewards_v164 import install_economy_rewards_v164
+from government_creator_sanctions_v164 import install_government_creator_sanctions_v164
 from government_release_v162 import install_government_release_v162
 
 
 install_government_release_v162(core)
+# Добавляет владельцу отдельную панель снятия всех санкций в Mini App Правительства.
+install_government_creator_sanctions_v164(core)
 # Финальные правила рейда ставятся после всех старых обёрток боя и героев.
 install_boss_rules_v163(core)
 install_boss_audio_v154()
