5f3c859bcda2938373f04f51861b09254dff1305
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-07-22 19:24:21 +0300

    Подключить Android WebView хотфикс Reality 166

--- talent_entry_v164.py
+++ talent_entry_v164.py
@@ -6,12 +6,15 @@ from boss_audio_v154 import install_boss_audio_v154
 from boss_rules_v163 import install_boss_rules_v163
 from boss_web_v163 import install_boss_web_v163
 from economy_rewards_v164 import install_economy_rewards_v164
+from government_android_webview_hotfix_v166 import install_government_android_webview_hotfix_v166
 from government_creator_sanctions_v164 import install_government_creator_sanctions_v164
 from government_release_v165 import install_government_release_v165
 
 
 # Финальный государственный слой включает запросы госструктур в казну Reality 165.
 install_government_release_v165(core)
+# Убирает параллельные фоновые загрузки и повторные DDL-запросы в Android WebView.
+install_government_android_webview_hotfix_v166(core)
 # Добавляет владельцу отдельную панель снятия всех санкций в Mini App Правительства.
 install_government_creator_sanctions_v164(core)
 # Финальные правила рейда ставятся после всех старых обёрток боя и героев.
