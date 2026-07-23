e48037f762a918b997abb0d4c11c6ac4a6eec5d3
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-07-23 16:51:49 +0300

    Добавить агрегатор имущества Reality 177

---
+++ government_reality_v177_property.py
@@ -0,0 +1,9 @@
+from government_reality_v177_property_actions import buy_property, insure_property, pay_property_debt, sell_to_state, set_primary, upgrade_property
+from government_reality_v177_property_auctions import bid_voluntary, process_voluntary_auctions, start_voluntary_auction
+from government_reality_v177_property_state import process_maintenance_v177, property_state_v177
+
+__all__ = [
+    "buy_property", "sell_to_state", "upgrade_property", "insure_property", "set_primary", "pay_property_debt",
+    "start_voluntary_auction", "bid_voluntary", "process_voluntary_auctions",
+    "property_state_v177", "process_maintenance_v177",
+]
