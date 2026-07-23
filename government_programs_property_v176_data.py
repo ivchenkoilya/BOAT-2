856b5e6dfd5102b365bd2b14f6e1d347b5a63980
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-07-23 15:28:36 +0300

    Подключить единый баланс фондов к госпрограммам Reality 176

--- government_programs_property_v176_data.py
+++ government_programs_property_v176_data.py
@@ -1,8 +1,8 @@
 from government_programs_property_v176_common import VERSION, PROGRAMS, PROPERTY_ITEMS, SCHEMA_SQL, ensure_schema, oversight_bonus
-from government_programs_property_v176_programs import run_program, apply_anti_crisis, process_expired_effects, expanded_oversight_report
+from government_programs_property_v176_funds import run_program, programs_state, apply_anti_crisis, process_expired_effects, expanded_oversight_report
 from government_programs_property_v176_property import buy_property, pay_property_debt, process_maintenance
 from government_programs_property_v176_enforcement import open_investigation, investigation_action, enact_property_bill, bid_auction, process_auctions, release_temporary_seizures
-from government_programs_property_v176_state import programs_state, property_state, refresh_declaration
+from government_programs_property_v176_state import property_state, refresh_declaration
 
 __all__ = [
     "VERSION", "PROGRAMS", "PROPERTY_ITEMS", "SCHEMA_SQL", "ensure_schema", "oversight_bonus",
